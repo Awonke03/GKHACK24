c476819712085bf24932168d34b3d73fb9605d96
Awonke03 <136753438+Awonke03@users.noreply.github.com>
2024-10-05 14:26:27 +0200

    Update gkhack24.py

--- gkhack24.py
+++ gkhack24.py
@@ -20,7 +20,7 @@ import statsmodels.api as sm
 from matplotlib import pyplot as plt
 
 # Load the data from the CSV file (adjust header as necessary)
-data = pd.read_csv("https://github.com/Awonke03/GKHACK24/blob/main/Water_Leakages.csv")
+data = pd.read_csv("https://raw.githubusercontent.com/Awonke03/GKHACK24/main/Water_Leakages.csv")
 
 # Display the first few rows of the updated DataFrame
 data.head()
@@ -151,7 +151,7 @@ df.head(2)
 df.shape
 
 #importing the continents dataset
-data2=pd.read_csv("continents-according-to-our-world-in-data.csv")
+data2=pd.read_csv(""https://raw.githubusercontent.com/Awonke03/GKHACK24/main/continents-according-to-our-world-in-data.csv"")
 data2.sample(n=5)
 
 # Merge the DataFrames based on 'Country Name' and 'Country Code'
@@ -393,7 +393,7 @@ fig3.update_layout(
 # Show the plot
 fig3.show()
 
-df5 = pd.read_excel("https://github.com/Awonke03/GKHACK24/blob/main/NC%20Data.xlsx")
+df5 = pd.read_excel(""https://raw.githubusercontent.com/Awonke03/GKHACK24/main/NC%20Data.xlsx"")
 
 # Sort the DataFrame by 'Agricultural Potential'
 northern_cape_df_sorted = df5.sort_values(by='Agricultural Potential', ascending=False)
@@ -414,7 +414,7 @@ fig4.update_layout(
 fig4.show()
 
 # Read data from the Excel file
-df = pd.read_excel('https://github.com/Awonke03/GKHACK24/blob/main/Leakage_Costs.xlsx')
+df = pd.read_excel("https://raw.githubusercontent.com/Awonke03/GKHACK24/main/Leakage_Costs.xlsx")
 
 df.columns = ['Year', 'Water Leakage (%)', 'Water Leakage Cost (Million R)']
 
@@ -442,7 +442,7 @@ fig5.update_layout(
 fig5.show()
 
 # Load the data from the Excel file
-file_path = 'https://github.com/Awonke03/GKHACK24/blob/main/Economic_Effect.xlsx'
+file_path = "https://raw.githubusercontent.com/Awonke03/GKHACK24/main/Economic_Effect.xlsx"
 df = pd.read_excel(file_path)
 
 # Filter the data for the relevant years (2018-2022)
